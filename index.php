e0f83a23213e30f3f89920752998ff0dafb7e205
babbandeep <babangurdaspur@gmail.com>
2017-05-04 00:04:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,32 @@
 </head>
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
+<?php
+	
+	require 'connect.php';
+
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
     Select image to upload:
-    <input type="text" name="title" id="title">
-    <input type="file" name="image" id="image">
+    <input type="text" name="title" id="title" required>
+    <input type="file" name="image" id="image" required>
     <input type="submit" value="Upload Image" name="submit">
 </form>
+
+<?php 
+
+	require 'upload.php';
+	require 'fetchimg.php';
+
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+	$(document).ready(function($){
+
+	});	
+</script>
 
 </body>
 </html>

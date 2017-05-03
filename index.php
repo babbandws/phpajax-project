90639311b0ee43f4d9c3121a6aa0f70a7bd2daab
babbandeep <babangurdaspur@gmail.com>
2017-05-03 13:52:06 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>php ajax project<title>
+</head>
+<body>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="text" name="title" id="title">
+    <input type="file" name="image" id="image">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>

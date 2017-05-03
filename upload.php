a1770fbe251f5cadf0e1ab3d58a3aa47cd818c39
babbandeep <babangurdaspur@gmail.com>
2017-05-04 00:05:47 +0530

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,8 +1,6 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 $name = $_POST['title'];
 $image = $_FILES['image']['name'];
@@ -13,10 +11,10 @@ $uploadOk = 1;
 
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
+if(isset($_POST["submit"])){
     $check = getimagesize($_FILES["image"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        //echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
         echo "File is not an image.";
@@ -45,60 +43,29 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
 
-    if(move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)){
-        $con = mysqli_connect($dbhost, $dbuser, $dbpass);
-        echo "The file ". basename( $_FILES["image"]["name"]). " has been uploaded.";
-    } else {
+    if(!move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)){
         echo "Sorry, there was an error uploading your file.";
     }
+
     if(!$con ){
          die('Could not connect: ' . mysqli_error($con));
     }    
 }
 
 
-// If we couldn't, then it either doesn't exist, or we can't see it.
-if (!mysqli_select_db($con, 'my_db')){
-    $sql = "CREATE DATABASE IF NOT EXISTS my_db";
-     if (mysqli_query($con, $sql)) {
-     echo "database created successfully";
-    } else {
-        echo "Error creating database: " . mysqli_error($con);
-    }
-}
-
-
-$sql = "CREATE TABLE IF NOT EXISTS my_db.images (
-        `id` int(100) unsigned NOT NULL AUTO_INCREMENT,
-        `title` varchar(255) NOT NULL,
-        `filename` varchar(255) NOT NULL,
-        `img_path` varchar(255) NOT NULL,
-        UNIQUE KEY `id` (`id`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;";
-
-
-if (mysqli_query($con, $sql)) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . mysqli_error($con);
-}
-
 $sql = "INSERT INTO images (title,filename,img_path)
-    VALUES ('$name','$image','$target_file')";
+    VALUES ('$name','$image','$target_file')";  
 
 $db_selected = mysqli_select_db($con, 'my_db');
 if (!$db_selected) {
-    die ('Can\'t use images : ' . mysqli_error($con));
+    die ('Database selection error : ' . mysqli_error($con));
 }
 
 $retval = mysqli_query($con, $sql);
 if(! $retval ){
   die('Could not enter data: ' . mysqli_error($con));
-} else {
-echo "Entered data successfully\n";
-mysqli_close($con);
+}   
+
 }
 
 ?>
-
-        

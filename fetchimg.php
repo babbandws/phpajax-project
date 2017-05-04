be9b0d4ffb902d51ce7a33469c922fec299f833d
babbandeep <babangurdaspur@gmail.com>
2017-05-04 07:44:14 +0530

    Update fetchimg.php

--- fetchimg.php
+++ fetchimg.php
@@ -1,17 +1,38 @@
 <?php
 
+require 'connect.php';
 
-$sql = "SELECT img_path,title FROM images ORDER BY id DESC";
-$sqlq = mysqli_query($con, $sql) or die(mysqli_error($con));   
-while($row = mysqli_fetch_array($sqlq, MYSQLI_BOTH)){
- 
+$sqlall = "SELECT COUNT(id) as num_rows FROM images WHERE id < ".$_POST['id']." ORDER BY id DESC";
+$sqlq = mysqli_query($con, $sqlall) or die(mysqli_error($con));   
+$row = mysqli_fetch_array($sqlq, MYSQLI_BOTH);
+$allRows = $row['num_rows'];
+
+$showLimit = 5;
+
+$query = mysqli_query($con, "SELECT id,title,img_path FROM images WHERE id < ".$_POST['id']." ORDER BY id DESC LIMIT ".$showLimit);
+
+$rowCount = mysqli_num_rows($query);
+
+if($rowCount > 0){ 
+    while($row = mysqli_fetch_array($query, MYSQLI_BOTH)){ 
+        $id = $row["id"]; ?>
 ?>
 
-<h1><?php echo $row["title"]; ?></h1>
-<img src="<?php echo $row["img_path"]; ?>" alt="">
+<div class="list_item"><h1><?php echo $row["title"]; ?></h1>
+<img src="<?php echo $row["img_path"]; ?>" alt=""></div>
 
 <?php 
 
+}
+
+if($allRows > $showLimit){ ?>
+    <div class="show_more_main" id="show_more_main<?php echo $id; ?>">
+        <span id="<?php echo $id; ?>" class=“show_more” title="Load more posts">Show more</span>
+        <span class="loding" style="display: none;"><span class="loding_txt">Loading….</span></span>
+    </div>
+
+<?php
+ } 
 }
 
 mysqli_close($con);

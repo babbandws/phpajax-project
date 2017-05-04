99d8075cdc138e1e4a5056e5a37b037cfd38e0aa
babbandeep <babangurdaspur@gmail.com>
2017-05-04 07:44:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <?php
 	
-	require 'connect.php';
+require 'connect.php';
 
 ?>
 
@@ -20,16 +20,61 @@
 
 <?php 
 
-	require 'upload.php';
-	require 'fetchimg.php';
+require 'upload.php';
+?>
+<div class="tutorial_list">
+
+<?php
+
+$sql = "SELECT id,title,img_path FROM images ORDER BY id DESC LIMIT 5";
+$sqlq = mysqli_query($con, $sql) or die(mysqli_error($con)); 
+$rowCount = mysqli_num_rows($sqlq);  
+
+if($rowCount > 0){ 
+	while($row = mysqli_fetch_array($sqlq, MYSQLI_BOTH)){
+
+	$id = $row['id'];
+ 
+?>
+
+<div class="list_item">
+<h1><?php echo $row["title"]; ?></h1>
+<img src="<?php echo $row["img_path"]; ?>" alt="">
+</div>
+
+<?php 
+
+}
 
 ?>
 
+<div class="show_more_main" id="show_more_main<?php echo $id; ?>">
+    <span id="<?php echo $id; ?>" class="show_more" title="Load more posts">Show more</span>
+    <span class="loding" style="display: none;"><span class="loding_txt">Loading....</span></span>
+</div>
+
+<?php } ?>
+
+</div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script>
-	$(document).ready(function($){
-
-	});	
+	$(document).ready(function(){
+    $(document).on('click','.show_more',function(){
+        var ID = $(this).attr('id');
+        $('.show_more').hide();
+        $('.loding').show();
+        $.ajax({
+            type:'POST',
+            url:'fetchimg.php',
+            data:'id='+ID,
+            success:function(html){
+                $('#show_more_main'+ID).remove();
+                $('.tutorial_list').append(html);
+            }
+        }); 
+    });
+});	
 </script>
 
 </body>
